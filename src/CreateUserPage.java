1c197750842e864b1af1e33892b55c9ad464b843
GrandpaOfAll <79862308+GrandpaOfAll@users.noreply.github.com>
2024-05-01 06:48:03 +0300

    File that contains all the banks created and read

--- src/CreateUserPage.java
+++ src/CreateUserPage.java
@@ -1,4 +1,10 @@
 import javax.swing.*;
 
 public class CreateUserPage extends JFrame {
+    private JPanel panel = new JPanel();
+    private JButton  createButton = new JButton("Create User");
+    private JButton loginButton = new JButton("Log In");
+    private JTextField name;
+    private JTextField ssn;
+    private JTextField salary;
 }
